4dfb6f4e5d2946fdd14826f82743dcaa6ddfbab9
jorvarea <jorvarea@users.noreply.github.com>
2024-03-23 12:54:26 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from print_board import print_board
 from valid_moves import valid_move
-
-def is_game_finished(board: list[str]):
-    is_finished = False
-    try:
-        board.index('K')
-        board.index('k')
-    except ValueError:
-        is_finished = True
-    return is_finished
-
-def execute_move(board: list[str], move: str) -> None:
-    piece = move[0]
-    next_position = int(move[1:])
-    position = board.index(piece)
-    board[position] = ' '
-    board[next_position] = piece
+from utils import is_game_finished
+from utils import execute_move
 
 def main() -> None:
     board = ['K', 'Q', 'R', 'B', 'N', 'P', ' ', ' ', ' ', ' ', 'p', 'n', 'b', 'r', 'q', 'k']
