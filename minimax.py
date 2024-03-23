85a4b56b1fe0aa44fd7b24d0ef5967a073bba287
jorvarea <jorvarea@users.noreply.github.com>
2024-03-23 12:56:45 +0100

    added minimax algorithm

---
+++ minimax.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from evaluation_function import evaluation_function
+from valid_moves import valid_move
+from utils import execute_move
+
+INFINITY = 999
+
+def child(board: list[str], move: str) -> list[str]:
+    new_board = board.copy()
+    execute_move(new_board, move)
+    return new_board
+
+def generate_possible_moves(board: list[str], to_move: str) -> set[str]:
+    pieces = "KQRNBP" if to_move == 'white' else "kqrnbp"
+    possible_moves: set[str] = set()
+    for piece in board:
+        if piece in pieces:
+            for position in range(16):
+                if valid_move(board, f"{piece}{position}"):
+                    possible_moves.add(f"{piece}{position}")
+    return possible_moves
+
+def minimax(board: list[str], depth: int, to_move: str) -> tuple[int, Optional[str]]:
+    if depth == 0:
+        result = evaluation_function(board), None
+    best_move = None
+    if to_move == 'white':
+        max_eval = -INFINITY
+        for move in generate_possible_moves(board, to_move):
+            new_board = child(board, move)
+            evaluation, _ = minimax(new_board, depth - 1, 'black')
+            if evaluation > max_eval:
+                max_eval = evaluation
+                best_move = move
+        result = max_eval, best_move
+    else:
+        min_eval = INFINITY
+        for move in generate_possible_moves(board, to_move):
+            new_board = child(board, move)
+            evaluation, _ = minimax(new_board, depth - 1, 'white')
+            if evaluation < min_eval:
+                min_eval = evaluation
+                best_move = move
+        result = min_eval, best_move
+    return result
