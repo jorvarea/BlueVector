14e514d0aacfd7808263dbfa661a269d2636b1d6
jorvarea <jorvarea@users.noreply.github.com>
2024-03-23 13:03:40 +0100

    minor fix

--- evaluation_function.py
+++ evaluation_function.py
@@ -11,7 +11,7 @@ def evaluation_function(board: list[str]) -> int:
     evaluation = 0
     for piece in board:
         if piece.isupper():
-            evaluation += PIECE_VALUE[piece]
+            evaluation += PIECE_VALUE[piece.lower()]
         else:
-            evaluation -= PIECE_VALUE[piece]
+            evaluation -= PIECE_VALUE[piece.lower()]
     return evaluation
